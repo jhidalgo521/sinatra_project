3056b8c15ebec9a1656a62a8e14bcabad0ee6f86
jhidalgo521 <jhidalgo521@gmail.com>
2020-08-16 16:18:04 -0400

    Start of Project

---
+++ spec/sinatra_project_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe SinatraProject do
+  it "has a version number" do
+    expect(SinatraProject::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
